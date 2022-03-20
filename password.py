b478c069d71ba8f150e397ad2202494af04dfb14
John Doe <aae36tw@gmail.com>
2022-03-20 08:28:15 +0800

    V1

--- password.py
+++ password.py
@@ -2,13 +2,17 @@ password = 'a123456'
 
 x = 3
 
-while True:
+while x > 0:
+    x = x - 1
     pwd = input('請輸入密碼')
     if pwd == password:
         print('登入正確')
         break
     else:
-        x = x - 1
-        print('登入錯誤，還剩', x, '次機會')
-        if x == 0:
-            break
+        print('登入錯誤')
+        if x > 0:
+            print('還有', x, '次機會')
+        else:
+            print('鎖帳號')            
+            
+        
